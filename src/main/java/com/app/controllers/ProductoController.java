f3f7ef30adb15a84991c41013feba3c39e240174
Sergio <sergiodanilomosqueraquigua@gmail.com>
2024-01-13 04:24:34 -0500

    cree la funcionalidad de guardar producto el usuario es quemado

--- src/main/java/com/app/controllers/ProductoController.java
+++ src/main/java/com/app/controllers/ProductoController.java
@@ -1,12 +1,25 @@
 package com.app.controllers;
 
+import com.app.models.Producto;
+import com.app.models.Usuario;
+import com.app.services.IProductoService;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/productos")
+@Slf4j
+@AllArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class ProductoController {
+
+    IProductoService productoService;
 
     @GetMapping("")
     public String show() {
@@ -16,5 +29,15 @@ public class ProductoController {
     @GetMapping("/create")
     public String create() {
         return "product/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Producto producto) {
+        Usuario usuario = new Usuario();
+        usuario.setId(1);
+        producto.setUsuario(usuario);
+        log.info("Producto: {}", producto);
+        productoService.save(producto);
+        return "redirect:/productos";
     }
 }
