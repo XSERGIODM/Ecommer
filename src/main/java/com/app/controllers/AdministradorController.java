2aba0687681a3554a4ea91db61e73e4df52af901
Sergio <sergiodanilomosqueraquigua@gmail.com>
2024-01-15 15:52:49 -0500

    Implementación las funcionalidades de cargar imagen y mostrar imagen en el home del admin

--- src/main/java/com/app/controllers/AdministradorController.java
+++ src/main/java/com/app/controllers/AdministradorController.java
@@ -1,15 +1,27 @@
 package com.app.controllers;
 
+import com.app.services.IProductoService;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/administrador")
+@Slf4j
+@AllArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class AdministradorController {
 
+    IProductoService productoService;
+
     @GetMapping("")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("listaProducto", productoService.findAll());
         return "admin/home";
     }
 }
