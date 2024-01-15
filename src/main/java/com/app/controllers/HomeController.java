ce461ee62a7056df782c4b1473860374f2c12f3d
Sergio <sergiodanilomosqueraquigua@gmail.com>
2024-01-15 17:44:57 -0500

    se agrego el home, homecontroller y template del usuario

---
+++ src/main/java/com/app/controllers/HomeController.java
@@ -0,0 +1,26 @@
+package com.app.controllers;
+
+import com.app.services.IProductoService;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@Slf4j
+@AllArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class HomeController {
+
+    IProductoService productoService;
+    @GetMapping("")
+    public String home(Model model){
+        model.addAttribute("listaProducto", productoService.findAll());
+        return "user/home";
+    }
+}
