bc68099908c41f052a7a6f5a863be70f47eeb7d6
Sergio <sergiodanilomosqueraquigua@gmail.com>
2024-01-15 20:30:50 -0500

    se dio la funcionalidad de obtener el producto y la cantidad para añadir al carrito

--- src/main/java/com/app/controllers/HomeController.java
+++ src/main/java/com/app/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.app.controllers;
 
+import com.app.models.DetalleOrden;
+import com.app.models.Orden;
 import com.app.models.Producto;
 import com.app.services.IProductoService;
 import lombok.AccessLevel;
@@ -8,10 +10,11 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -21,19 +24,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     IProductoService productoService;
+    List<DetalleOrden> detalleOrdenList = new ArrayList<>();
+    Orden orden = new Orden();
+
     @GetMapping("")
-    public String home(Model model){
+    public String home(Model model) {
         model.addAttribute("listaProducto", productoService.findAll());
         return "user/home";
     }
+
     @GetMapping("productoHome/{id}")
-    public String productoHome(@PathVariable (value = "id") Integer id, Model model){
+    public String productoHome(@PathVariable(value = "id") Integer id, Model model) {
         model.addAttribute("producto", productoService.findById(id).get());
         return "user/productoHome";
     }
 
     @PostMapping("/cart")
-    public String cart(){
+    public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Producto producto = new Producto();
+        double sumaTotal = 0;
+        Optional<Producto> optionalProducto = productoService.findById(id);
+        log.info("Producto añadido: {}", optionalProducto.get());
+        log.info("Cantidad: {}", cantidad);
+
         return "user/carrito";
     }
 
